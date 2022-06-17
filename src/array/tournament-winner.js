b121a2290bf14988764d9140c694316f9ffab4f7
Sherry Zheng <sherryzheng2018@gmail.com>
2022-06-17 16:16:13 -0700

    tournament winner

---
+++ src/array/tournament-winner.js
@@ -0,0 +1,17 @@
+function tournamentWinner(competition, results) {
+    let winner = '';
+    let score = {[winner] : 0};
+
+    for (let i = 0; i < competition.length; i++) {
+        const [homeTeam, awayTeam] = competition[i];
+        const winningTeam = results[i] === 1 ? homeTeam : awayTeam;
+
+        // here using "Nullish Coalescing"
+        // source from https://www.freecodecamp.org/news/how-the-question-mark-works-in-javascript/
+        score[winningTeam] = (score[winningTeam] ?? 0) + 3;
+        if (score[winningTeam] > score[winner] ? 0) {
+            winner = winningTeam;
+        }
+    }
+    return winner;
+}
