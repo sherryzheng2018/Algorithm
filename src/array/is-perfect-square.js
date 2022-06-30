8b965a419a45b891ac861c5d2510e7ccf39e9c4d
Sherry Zheng <sherryzheng2018@gmail.com>
2022-06-30 10:58:50 -0700

    update is perfect square solution

---
+++ src/array/is-perfect-square.js
@@ -0,0 +1,17 @@
+// Write code to create a function that accepts a number and returns true if the number
+// is a perfect square, otherwise it returns false.
+// You may not use the built-in Math.sqrt method
+
+var isPerfectSquare = function(num) {
+    let i = 0;
+    while (true) {
+        let currentSquare = i * i;
+        if (currentSquare === num) {
+            return true;
+        } else if (currentSquare > num) {
+            return false;
+        } else {
+            i++;
+        }
+    }
+};
